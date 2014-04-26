c66e51f3a1c39fd9b2c9dd33c72725bad8a00fbd
mrussellclarke <mrussellclarke@gmail.com>
2014-04-26 14:43:48 -0400

    Add BisonMaps.java

--- src/BisonMaps.java
+++ src/BisonMaps.java
@@ -16,12 +16,12 @@ public class BisonMaps {
 
         // read in the graph from a file
         In graphin = new In(args[0]); //Declares new object graphin of type In and sets value to function In with arguments called with program 
-        EuclideanGraph G = new EuclideanGraph(graphin);
+        EuclideanGraph G = new EuclideanGraph(graphin);//Declares object G of type EuclideanGraph and calls with file graphin
         System.err.println("Done reading the graph " + args[0]);
         System.err.println("Enter query pairs from stdin");
 
         // read in the s-d pairs from standard input
-        Dijkstra dijkstra = new Dijkstra(G);
+        Dijkstra dijkstra = new Dijkstra(G);//Declares new object dijkstra of type Dijkstra and sets value to result of calling Dijkstra with argument G
         while(!StdIn.isEmpty()) {
             int s = StdIn.readInt();
             int d = StdIn.readInt();
